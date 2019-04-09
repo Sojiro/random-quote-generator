195345c6fcca5b4dd8482c4a2c5a8b098e0398aa
Ashok M A <ashok5angel@gmail.com>
2019-04-09 23:20:47 +0200

    Random Quote Generator API :MVP

---
+++ data/quotes.js
@@ -0,0 +1,40 @@
+const Quotes = [
+  {
+    quote:
+      'I have noticed even people who claim everything is predestined, and that we can do nothing to change it, look before they cross the road.',
+    author: 'Stephen Hawking'
+  },
+  {
+    quote:
+      'Bad programmers worry about the code. Good programmers worry about data structures and their relationships.',
+    author: 'Linus Torvalds, Creator of Linux and Git'
+  },
+  {
+    quote: 'Listen to many, speak to a few.',
+    author: 'William Shakespeare'
+  },
+  {
+    quote:
+      'To keep the body in good health is a duty... otherwise we shall not be able to keep our mind strong and clear.',
+    author: 'Buddha'
+  },
+  {
+    quote:
+      'You will not be punished for your anger, you will be punished by your anger.',
+    author: 'Buddha'
+  },
+  {
+    quote: 'Quality is not an act, it is a habit.',
+    author: 'Aristotle'
+  },
+  {
+    quote: 'A friend to all is a friend to none.',
+    author: 'Aristotle'
+  },
+  {
+    quote: 'Anyone who has never made a mistake has never tried anything new.',
+    author: 'Albert Einstein'
+  }
+];
+
+module.exports = Quotes;
